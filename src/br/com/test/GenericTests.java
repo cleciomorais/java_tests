483bc9feda929c7a0c909ff8dc4a23ba576452cb
cleciomorais <cleciovmorais@gmail.com>
2021-12-02 23:33:51 -0300

    primeiro commit no branch main

---
+++ src/br/com/test/GenericTests.java
@@ -0,0 +1,13 @@
+package br.com.test;
+
+public class GenericTests {
+
+	public static void main(String[] args) {
+		printMain();
+	}
+	
+	private static void printMain() {
+		System.out.println("print main branch method");
+	}
+
+}
