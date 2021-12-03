f1f6755427e5f16d24a089bfa799002a9e73644c
cleciomorais <cleciovmorais@gmail.com>
2021-12-02 23:38:19 -0300

    primeiro commit no branch b01

--- src/br/com/test/GenericTests.java
+++ src/br/com/test/GenericTests.java
@@ -10,5 +10,9 @@ public class GenericTests {
 		System.out.println("print main branch method - linha 1");
 		System.out.println("print main branch method - linha 2");
 	}
+	
+	private static void printB01() {
+		System.out.println("print b01 branch method");
+	}
 
 }
