892e951c088e34b4cfc0fe83749b0d10d0041a8d
cleciomorais <cleciovmorais@gmail.com>
2021-12-03 00:00:35 -0300

    quarto commit em main

--- src/br/com/test/GenericTests.java
+++ src/br/com/test/GenericTests.java
@@ -11,6 +11,7 @@ public class GenericTests {
 		System.out.println("print main branch method - linha 1");
 		System.out.println("print main branch method - linha 2");
 		System.out.println("print main branch method - linha 3");
+		System.out.println("print main branch method - linha 4");
 	}
 	
 	private static void printB01() {
