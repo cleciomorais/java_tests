14d4a4d54122d8c8fe3431dd901a7787adbe03a5
cleciomorais <cleciovmorais@gmail.com>
2021-12-07 10:39:03 -0300

    commit para inicio do teste no netbeans a classe ficou somente com o metodo main e printMain com uma linha de sysout

--- src/br/com/test/GenericTests.java
+++ src/br/com/test/GenericTests.java
@@ -4,40 +4,10 @@ public class GenericTests {
 
 	public static void main(String[] args) {
 		printMain();
-		printB01();
-		printB02();
-		printB03();
 	}
 	
 	private static void printMain() {
 		System.out.println("print main branch method - linha 1");
-		System.out.println("print main branch method - linha 2");
-		System.out.println("print main branch method - linha 3");
-		System.out.println("print main branch method - linha 4");
-		System.out.println("print main branch method - linha 5");
-		System.out.println("print main branch method - linha 6");
-		System.out.println("print main branch method - linha 7");
-		System.out.println("print main branch method - linha 8");
-		System.out.println("print main branch method - linha 9");
-                System.out.println("print main branch method - linha 10.2");
 	}
 	
-	private static void printB01() {
-		System.out.println("print b01 branch method - linha 1");
-		System.out.println("print b01 branch method - linha 2");
-		System.out.println("print b01 branch method - linha 3");
-	}
-
-	private static void printB02() {
-		System.out.println("print b02 branch method - linha 1");
-		System.out.println("print b02 branch method - linha 2");
-		System.out.println("print b02 branch method - linha 3");
-		System.out.println("print b02 branch method - linha 4");
-	}
-	
-	private static void printB03() {
-		System.out.println("print b03 branch method - linha 1");
-		System.out.println("print b03 branch method - linha 2");
-		System.out.println("print b03 branch method - linha 3");
-	}
 }
