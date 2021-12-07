1017f34e97a0e7a9ed173f25d39f13be2918019b
cleciomorais <cleciovmorais@gmail.com>
2021-12-07 11:57:22 -0300

    segundo commit no main apos criação do nb01

--- src/br/com/test/GenericTests.java
+++ src/br/com/test/GenericTests.java
@@ -7,11 +7,14 @@ public class GenericTests {
 	}
 	
 	private static void printMain() {
+            
 		System.out.println("primeiro commit no branch main local netbeans");
                 
                 System.out.println("segundo commit no branch main local netbeans");
                 
                 System.out.println("primeiro commit no branch main apos criação do branch nb01");
+                
+                System.out.println("segundo commit no branch main apos criação do branch nb01");
 	}
 	
 }
