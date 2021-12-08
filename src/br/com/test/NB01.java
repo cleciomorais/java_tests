491d5917b3193fcc9b8604885728389f9455d0a4
cleciomorais <cleciovmorais@gmail.com>
2021-12-08 08:08:55 -0300

    Revert "commit em ec02"
    This reverts commit 5a6f0bb0ae37e06842afe075e31470645ace86a2.

--- src/br/com/test/NB01.java
+++ src/br/com/test/NB01.java
@@ -11,10 +11,10 @@ public class NB01 {
         int arr[] = {1,5,7,9,3,2};
         
     	for (int i : arr) {
-    		System.out.println("valor array em [" + i + "] : " + i);
+    		System.out.println("valor array em [" + i + "] :" + i);
 		}
     	
-    	System.out.println("processo finalizado em newPrintNB01() da classe NB01.java");
+    	System.out.println("processo finalizado em newPrintNB01()");
         
     }
 }
